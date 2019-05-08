e2ad933967629b2d222a6dc6b759cb9a847d4175
hobetuz <hobetuz@ipartekaula.com>
2019-05-08 20:04:30 +0200

    Añadido detalles cliente y Uppercase

--- src/app/clientes/clientes.component.ts
+++ src/app/clientes/clientes.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Cliente } from '../cliente';
 
 @Component({
   selector: 'app-clientes',
@@ -6,6 +7,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./clientes.component.css']
 })
 export class ClientesComponent implements OnInit {
+
+  cliente: Cliente = {
+    id: 1,
+    name: 'Oscar',
+    nif : 'P19929233',
+  };
 
   constructor() { }
 
