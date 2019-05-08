29bad25eb499783bb2968df71b41c180eb1bc840
hobetuz <hobetuz@ipartekaula.com>
2019-05-08 20:15:37 +0200

    Creado Mock Clientes

---
+++ src/app/mock-clientes.ts
@@ -0,0 +1,11 @@
+import { Cliente } from './cliente';
+export const HEROES: Cliente[] = [
+  { id: 11, name: 'Oscar' , nif: '23456789P' },
+  { id: 12, name: 'Pepe' , nif: '23456789P' },
+  { id: 13, name: 'Natxo' , nif: '23456789P' },  
+  { id: 14, name: 'Jorge' , nif: '23456789P' },
+  { id: 15, name: 'Ainhoa' , nif: '23456789P' },
+  { id: 16, name: 'Raul' , nif: '23456789P' },
+  { id: 17, name: 'Laura' , nif: '23456789P' },
+  { id: 18, name: 'Aitor' , nif: '23456789P' },
+];
