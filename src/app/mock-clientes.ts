53cd614a77ba5eb9ab3b39e214c1b17cac25e086
hobetuz <hobetuz@ipartekaula.com>
2019-05-09 19:27:24 +0200

    Modificacion de Nifs de clientes en mock

--- src/app/mock-clientes.ts
+++ src/app/mock-clientes.ts
@@ -1,11 +1,11 @@
 import { Cliente } from './cliente';
 export const CLIENTES: Cliente[] = [
   { id: 11, name: 'Oscar' , nif: '23456789P' },
-  { id: 12, name: 'Pepe' , nif: '23456789P' },
-  { id: 13, name: 'Natxo' , nif: '23456789P' },  
-  { id: 14, name: 'Jorge' , nif: '23456789P' },
-  { id: 15, name: 'Ainhoa' , nif: '23456789P' },
-  { id: 16, name: 'Raul' , nif: '23456789P' },
-  { id: 17, name: 'Laura' , nif: '23456789P' },
-  { id: 18, name: 'Aitor' , nif: '23456789P' },
+  { id: 12, name: 'Pepe' , nif: '13432789P' },
+  { id: 13, name: 'Natxo' , nif: '74346789P' },  
+  { id: 14, name: 'Jorge' , nif: '96756789P' },
+  { id: 15, name: 'Ainhoa' , nif: '783456789P' },
+  { id: 16, name: 'Raul' , nif: '45556789P' },
+  { id: 17, name: 'Laura' , nif: '23765789P' },
+  { id: 18, name: 'Aitor' , nif: '14556789P' },
 ];
