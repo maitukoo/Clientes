128d527deda35aa3d0b9350fcdc737a845039357
hobetuz <hobetuz@ipartekaula.com>
2019-05-09 20:04:45 +0200

    Añadido messageservice en cliente service

--- src/app/cliente.service.ts
+++ src/app/cliente.service.ts
@@ -2,15 +2,17 @@ import { Injectable } from '@angular/core';
 import { Cliente } from './cliente';
 import { CLIENTES } from './mock-clientes';
 import { Observable, of } from 'rxjs';
+import { MessageService } from './message.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ClienteService {
 
-  constructor() { }
+  constructor(private messageService: MessageService) { }
 
   getClientes() : Observable<Cliente[]>{
+    this.messageService.add('ClienteService: fetched heroes');
     return of(CLIENTES);
   }
 }
