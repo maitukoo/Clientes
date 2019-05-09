5e23124f1516b2f8f20846affcd4e45fb45e8772
hobetuz <hobetuz@ipartekaula.com>
2019-05-09 19:35:15 +0200

    La tabla de clientes mola mogollon

--- src/app/cliente.service.ts
+++ src/app/cliente.service.ts
@@ -1,4 +1,6 @@
 import { Injectable } from '@angular/core';
+import { Cliente } from './cliente';
+import { CLIENTES } from './mock-clientes';
 
 @Injectable({
   providedIn: 'root'
@@ -6,4 +8,8 @@ import { Injectable } from '@angular/core';
 export class ClienteService {
 
   constructor() { }
+
+  getClientes() : Cliente[]{
+    return CLIENTES;
+  }
 }
