6bcdd53ba1e6801f3e43ccd7b18cacce227b8185
hobetuz <hobetuz@ipartekaula.com>
2019-05-09 18:55:41 +0200

    Generado componente de detalle de cliente

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,13 @@ import { FormsModule } from '@angular/forms'; // <-- NgModel lives here
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ClientesComponent } from './clientes/clientes.component';
+import { ClienteDetalleComponent } from './cliente-detalle/cliente-detalle.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ClientesComponent
+    ClientesComponent,
+    ClienteDetalleComponent
   ],
   imports: [
     BrowserModule,
